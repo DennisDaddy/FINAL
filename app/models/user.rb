54f80505abd6815003646916db73747537a51712
DennisDaddy <softteez95@gmail.com>
2017-01-20 11:41:16 +0300

    Adding a remember method to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-before_save { self.email = email.downcase }
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
  validates :name,  presence: true, length: { maximum: 50 }
  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -18,6 +19,12 @@ before_save { self.email = email.downcase }
   # Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
 
