a905aa6ed5f8cd9e8cca82a1e45b884c6a6942a3
DennisDaddy <softteez95@gmail.com>
2017-01-23 09:06:52 +0300

    remove error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  
+    include ApplicationHelper
+
+
+
 
   # Returns true if a test user is logged in.
   def is_logged_in?
@@ -30,7 +33,6 @@ class ActiveSupport::TestCase
   end
 
 
-   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
