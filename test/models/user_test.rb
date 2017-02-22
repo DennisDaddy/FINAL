72ec55b6ff8e0f5c4b4667a22cf1c3952daa4ef3
DennisDaddy <softteez95@gmail.com>
2017-02-22 15:50:32 +0300

    add include

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -89,7 +89,7 @@ class UserTest < ActiveSupport::TestCase
     archer  = users(:archer)
     assert_not michael.following?(archer)
     michael.follow(archer)
-    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
